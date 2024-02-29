963879e73ee6db7bc190b3f06602d83c112173b1
Pedro Moreno <codigo314@gmail.com>
2024-02-28 19:07:52 -0500

    inicio

---
+++ src/helpers/getGifs.js
@@ -0,0 +1,12 @@
+export const getGifs = async (category) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=rmy349QafZ0fRIFX8wRfZc38C7pCaFG1&limit=25&q=${category}`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+
+  const gifs = data.map((img) => ({
+    id: img.id,
+    title: img.title,
+    url: img.images.downsized_medium.url,
+  }));
+  return gifs;
+};
